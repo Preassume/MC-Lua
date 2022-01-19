33523ad2ef2c6e84475d8eeddae1e8352d74d721
preassume <aweraa29@gmail.com>
2022-01-19 10:56:35 -0600

    updated script list

--- scriptLocations.lua
+++ scriptLocations.lua
@@ -1,6 +1,7 @@
 local scriptLocations = {
     ["commonAPI"] = "commonAPI.lua",
     ["autoAcacia"] = "treeFarming/autoAcacia.lua",
+    ["autoTreeV3"] = "treeFarming/autoTreeV3.lua",
     ["autoFarm"] = "cropFarming/autoFarm.lua",
 }
 
