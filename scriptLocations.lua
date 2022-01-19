72dbac5af8f7c7abbb0cfc14070070e13f2d1afc
preassume <aweraa29@gmail.com>
2022-01-19 11:02:08 -0600

    added acacia

--- scriptLocations.lua
+++ scriptLocations.lua
@@ -2,6 +2,7 @@ local scriptLocations = {
     ["commonAPI"] = "commonAPI.lua",
     ["autoAcacia"] = "treeFarming/autoAcacia.lua",
     ["autoTreeV3"] = "treeFarming/autoTreeV3.lua",
+    ["acacia"] = "treeFarming/acacia.lua",
     ["autoFarm"] = "cropFarming/autoFarm.lua",
 }
 
