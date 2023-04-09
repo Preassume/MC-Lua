8c1d97070c3d5b2e22af3796a645c890a5ab0ba1
preassume <aweraa29@gmail.com>
2023-04-08 23:51:34 -0500

    changed key, fixes

--- lua-scripts/elevatorScripts/returner.lua
+++ lua-scripts/elevatorScripts/returner.lua
@@ -8,16 +8,16 @@ local modem = peripheral.wrap("left")
 local sendCh = tonumber(arg[1])
 
 while true do
-    print("Press 'y' to send the elevator back up:")
+    print("Press 'enter' to send the elevator back up:")
     local event, key = os.pullEvent("key_up")
     local name = keys.getName(key)
-    if key == keys.y then
+    if key == keys.enter then
         term.clear()
         print("Going up!")
         sleep(1)
-        modem.transmit(ch, 0, "up")
+        modem.transmit(sendCh, 0, "up")
+        sleep(2)
+        term.clear()
+        sleep(1)
     end
-    sleep(2)
-    term.clear()
-    sleep(1)
 end
